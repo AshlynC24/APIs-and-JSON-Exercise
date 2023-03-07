f12caaa776038a984a0dc3181eac0cc1965333a8
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-03-06 19:10:12 -0500

    the convo

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using KanyeAndRon;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Quotes.KanyeQuote();
+
+            Quotes.RonQuote();
+            
         }
     }
 }
---
+++ APIsAndJSON/Quotes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+	class Quotes
+	{
+		public static void KanyeQuote()
+		{
+			var client = new HttpClient();
+
+			var kanyeURL = "https://api.kanye.rest/";
+
+			var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+			var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+			Console.WriteLine($"--------------------------------");
+			Console.WriteLine($"Kanye: {kanyeQuote}");
+			Console.WriteLine("----------------------------------");
+
+        }
+		public static void RonQuote()
+		{
+			var client = new HttpClient();
+
+			var RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+			var RonResponse = client.GetStringAsync(RonURL).Result;
+
+			var RonQuote = JArray.Parse(RonResponse).ToString().Replace('[',' ').Replace(']',' ').Trim();
+
+			Console.WriteLine($"------------------------------");
+			Console.WriteLine($"Ron: {RonQuote}");
+			Console.WriteLine("--------------------------------");
+
+        }
+
+	}
+}
+
